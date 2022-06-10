a35a2a828cb162480ad1d303af20fe1b1ed12fe2
Adam G <37425558+aglucky@users.noreply.github.com>
2022-06-10 11:18:16 -0400

    filter

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 
     
 
-    application.add_handler(get_handler)
+    application.add_handler(get_handler, filters.user(username="@agluck1"))
     application.add_handler(add_handler)
     application.add_handler(del_handler)
 
