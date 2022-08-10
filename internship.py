193796eb293c608e50e3eb4595e8a9f3f5e6b536
Adam G <37425558+aglucky@users.noreply.github.com>
2022-08-10 17:00:23 -0400

    formtting 2

--- internship.py
+++ internship.py
@@ -7,13 +7,15 @@ def queryInternship(query, pos):
     results = [x for x in response.json() if x['company'].lower() == query.lower()]
     text = ""
     try:
-        for i in results[pos:pos+5]:
+        for i in results[pos:min(pos+3, len(results)-1)]:
             text += f"""- {i['company']}, {i['season']} {i['yr']}
                         Location: {i['loc']}
                         Pay: {i['hourlySalary']}/hr or {i['monthlySalary']}"
                         More info: {i['moreInfo']}\n\n"""
     except:
+        text = "Error handling request"
+    if text == "":
         text = "No results found"
     return text
 
-print(queryInternship("google", 0))
+print(queryInternship("ffffsa", 10))
