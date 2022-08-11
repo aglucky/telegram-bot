231611f401718e14ca8e6f3a506c4a6bee4cd281
Adam G <37425558+aglucky@users.noreply.github.com>
2022-08-10 21:02:10 -0400

    fixed search edge case

--- internship.py
+++ internship.py
@@ -1,4 +1,5 @@
 import requests
+from collections import defaultdict
 
 
 def queryInternship(query, pos):
@@ -7,15 +8,15 @@ def queryInternship(query, pos):
     results = [x for x in response.json() if x['company'].lower() == query.lower()]
     text = ""
     try:
-        for i in results[pos:min(pos+3, len(results)-1)]:
-            text += f"""- {i['company']}, {i['season']} {i['yr']}
-                        Location: {i['loc']}
-                        Pay: {i['hourlySalary']}/hr or {i['monthlySalary']}"
-                        More info: {i['moreInfo']}\n\n"""
+        for i in results[pos:min(pos+3, len(results))]:
+            text += f"""- {i.setdefault('company', 'N/A')}, {i['season']} {i['yr']}
+                        Location: {i.setdefault('loc', 'N/A')}
+                        Pay: {i['hourlySalary']}/hr or {i['monthlySalary']}/month
+                        More info: {i.setdefault('moreInfo','N/A')}\n\n"""
     except:
         text = "Error handling request"
     if text == "":
         text = "No results found"
     return text
 
-print(queryInternship("ffffsa", 10))
+print(queryInternship("amazon", 0))
